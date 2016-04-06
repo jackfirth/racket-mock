bef97d8fda1cce2dfe40da178bbf412f05964a38
Rick van de Loo <rickvandeloo@gmail.com>
2016-04-06 22:41:28 +0200

    check-mock-called-with? prints expected and actual
    display expected and actual calls when check-mock-called-with? fails

--- mock/private/check.rkt
+++ mock/private/check.rkt
@@ -11,8 +11,13 @@ provide
     check-mock-num-calls (-> exact-nonnegative-integer? mock? void?)
 
 
-(define-simple-check (check-mock-called-with? args mock)
-  (mock-called-with? args mock))
+(define-check (check-mock-called-with? args mock)
+  (let ([result (mock-called-with? args mock)])
+    (with-check-info
+      [('expected args) ('actual (mock-calls mock))]
+      (unless result
+        (fail-check "Actual arguments did not match expected")))))
+
 
 (define-simple-check (check-mock-num-calls expected-num-calls mock)
   (equal? (mock-num-calls mock) expected-num-calls))
