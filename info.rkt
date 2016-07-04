986cf437fa07b3e6aa34070bbe65b993ec478cd4
Jack Firth <jackhfirth@gmail.com>
2016-07-04 16:13:27 -0700

    Collapse info file whitespace
    Style-wise, I no longer like blank lines in info files, they have so
    little content it just seems like it gets in the way.

--- info.rkt
+++ info.rkt
@@ -1,12 +1,6 @@
 #lang info
-
-
 (define collection 'multi)
-
-
 (define version "0.1")
-
-
 (define deps
   '("base"
     "fancy-app"
@@ -14,16 +8,12 @@
     "scribble-lib"
     "sweet-exp"
     "unstable-lib"))
-
-
 (define build-deps
   '("cover"
     "rackunit-lib"
     "rackunit-doc"
     "racket-doc"
     "doc-coverage"))
-
-
 (define test-omit-paths
   '("mock/main.scrbl"
     "mock/private/base.scrbl"
