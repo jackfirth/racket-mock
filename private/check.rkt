57b98c9cde8906d3d4986816d4d61e104aef8f50
Jack Firth <jackhfirth@gmail.com>
2016-07-12 01:10:12 -0700

    Test raising of failure when mock not called

--- private/check.rkt
+++ private/check.rkt
@@ -10,12 +10,26 @@ provide
     check-mock-called-with? (-> mock? arguments? void?)
     check-mock-num-calls (-> exact-nonnegative-integer? mock? void?)
 
+module+ test
+  require racket/function
+          rackunit
+
+
+(define (fail-unless-called mock args)
+  (unless (mock-called-with? mock args)
+    (fail-check no-calls-made-message)))
+
+(module+ test
+  (check-exn exn:test:check?
+             (thunk (fail-unless-called (mock) (arguments 1 2 3)))))
+
+(define no-calls-made-message
+  "No calls were made matching the expected arguments")
 
 (define-check (check-mock-called-with? mock args)
   (with-check-info (['expected-args args]
                     ['actual-calls (mock-calls mock)])
-    (unless (mock-called-with? mock args)
-      (fail-check "No calls were made matching the expected arguments"))))
+    (fail-unless-called mock args)))
 
 (define-simple-check (check-mock-num-calls expected-num-calls mock)
   (equal? (mock-num-calls mock) expected-num-calls))
