6b61aa7064b864bc98a6d48f60d158884f9d7493
Jack Firth <jackhfirth@gmail.com>
2016-07-29 00:20:58 -0700

    Bump package version
    New feature: current-mock-name

--- mock/info.rkt
+++ mock/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "mock")
 (define scribblings '(("main.scrbl" (multi-page) (library) "mock")))
-(define version "1.0")
+(define version "1.1")
 (define deps
   '(("base" #:version "6.4")
     "fancy-app"
