b0f2be5db7bd6702987a55d80ba57a5722831686
Jack Firth <jackhfirth@gmail.com>
2017-01-17 02:01:00 -0800

    Add exception raising function utilities

--- mock/private/function.rkt
+++ mock/private/function.rkt
@@ -26,3 +26,23 @@ module+ test
 
 (module+ test
   (check-equal? (void/kw #:foo 'arg) (void)))
+
+(define (const-raise v)
+  (thunk* (raise v)))
+
+(module+ test
+  (struct foo ())
+  (check-exn foo? (const-raise (foo)))
+  (check-exn foo? (thunk ((const-raise (foo)) 'arg)))
+  (check-exn foo? (thunk ((const-raise (foo)) #:foo 'arg))))
+
+(define (const-raise-exn #:message [msg "failure"]
+                         #:constructor [exn-constructor make-exn:fail])
+  (thunk* (raise (exn-constructor msg (current-continuation-marks)))))
+
+(module+ test
+  (check-exn #rx"failure" (const-raise-exn))
+  (check-exn exn:fail? (const-raise-exn))
+  (check-exn #rx"custom message" (const-raise-exn #:message "custom message"))
+  (struct custom-exn exn:fail () #:transparent)
+  (check-exn custom-exn? (const-raise-exn #:constructor custom-exn)))
